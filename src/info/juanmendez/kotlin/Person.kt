c96a5fba01ef7c89bbb7ddb0d68cf3cc02f15a6b
Juan Mendez <contact@juanmendez.info>
2017-09-01 16:29:44 -0500

    saving Person

---
+++ src/info/juanmendez/kotlin/Person.kt
@@ -0,0 +1,11 @@
+package info.juanmendez.kotlin
+
+/**
+ * Created by musta on 9/1/2017.
+ */
+
+//classes can be declared with constructors
+//long way: Person constructor(firstName:String)
+class Person (firstName:String){
+    val upperFirstName = firstName.toUpperCase()
+}
