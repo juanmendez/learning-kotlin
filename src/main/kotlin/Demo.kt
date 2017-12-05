728b8a11c2075ea6a968df86f24f85161ee18178
Juan Mendez <contact@juanmendez.info>
2017-12-05 11:23:17 -0600

    adding main

--- src/main/kotlin/Demo.kt
+++ src/main/kotlin/Demo.kt
@@ -2,3 +2,10 @@
  * Created by musta on 10/23/2017.
  */
 fun start(): String = "hello"
+
+
+//here is how we start with a static main method
+
+fun main( args:Array<String>){
+    println( start() )
+}
