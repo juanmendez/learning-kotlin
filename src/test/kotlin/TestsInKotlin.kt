3925ea85d2dd9abf988f0e5fe8763711bae39c35
Juan Mendez <contact@juanmendez.info>
2018-01-27 12:58:09 -0600

    sequences

--- src/test/kotlin/TestsInKotlin.kt
+++ src/test/kotlin/TestsInKotlin.kt
@@ -57,4 +57,34 @@ class Tests {
     //custom predicates, help elaborate reusable predicates rather than being inline
     fun predicateByBandName(band:Band, name:String )=band.name.equals( name )
 
+
+    @Test
+    fun `asSequenceTest`(){
+        val bands = getBandSequence("./raw/great_nineties_albums.csv")
+        assertTrue( bands.count() > 0 )
+
+        //all albums are in the 90's
+        assertTrue( bands.all { it.year.toString().contains( Regex("199")) } )
+
+        //two albums belong to Soundgarden
+        assertEquals( bands.count({ it.name.equals("Soundgarden")}), 2 )
+    }
+
+    /**
+     * Outer loop iterates elements, inner loop iterates transformations
+     * Without sequence, outer loop iterates transformations, inner loop iterates elements
+     *
+     * Sequences are useful for streaming or very large lists
+     */
+    fun getBandSequence( fileLocation: String ): Sequence<Band> {
+        val file = File(fileLocation)
+
+        return file.readLines().asSequence().drop(1) //skip header row
+                .map { it.split(",") } //make row a list of strings split by comma
+                .map { Band().apply { //map each col to band attribute
+                    name = it[0]
+                    album = it[1]
+                    year = it[2].toInt()
+                }} //0. mapping turned initial list into List<Band>
+    }
 }
